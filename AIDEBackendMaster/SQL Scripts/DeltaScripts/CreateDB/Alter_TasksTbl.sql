c6849d080a59a4d6e847ae8889bf1398747997fe
johnharveysanchez7 <harvex007@gmail.com>
2020-01-09 12:25:38 +0800

    AIDE-BACKEND-412 Project - Create long project name is truncated #412

---
+++ AIDEBackendMaster/SQL Scripts/DeltaScripts/CreateDB/Alter_TasksTbl.sql
@@ -0,0 +1,16 @@
+USE [AIDE]
+GO
+
+/****** Object:  Table [dbo].[TASKS]    Script Date: 09/02/2019 1:53:26 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER TABLE TASKS 
+ALTER COLUMN REF_ID varchar(10)
+
+GO
+
+
