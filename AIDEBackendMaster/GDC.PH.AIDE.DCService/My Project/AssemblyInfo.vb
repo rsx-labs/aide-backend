13828c482801af5e62be564f4bb1e6925357928a
trashvin <m3lles@gmail.com>
2020-05-22 23:10:54 +0800

    Add installer solution.

--- AIDEBackendMaster/GDC.PH.AIDE.DCService/My Project/AssemblyInfo.vb
+++ AIDEBackendMaster/GDC.PH.AIDE.DCService/My Project/AssemblyInfo.vb
@@ -8,17 +8,17 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("GDC.PH.AIDE.DCService")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("GDC.PH.AIDE.DCService")> 
-<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTitle("AIDE Backend Service")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("RS Experimental Labs")>
+<Assembly: AssemblyProduct("AIDE Backend Service")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("d10ec658-a7a6-4aec-b395-c09f0137c311")> 
+<Assembly: Guid("d10ec658-a7a6-4aec-b395-c09f0137c311")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.3.0.0")> 
-<Assembly: AssemblyFileVersion("3.3.0.0")> 
+<Assembly: AssemblyVersion("3.3.3.0")>
+<Assembly: AssemblyFileVersion("3.3.3.0")>
